1e1a0864f302d8975597ca4cfbdeac13e3f373a0
Kauan494 <kauan.elias.ribeiro@gmail.com>
2026-05-09 17:03:23 -0300

    aula

---
+++ Aula_POO/Aula12_ex1/src/aula12_ex1/Aula12_ex1.java
@@ -0,0 +1,12 @@
+package aula12_ex1;
+
+
+public class Aula12_ex1 {
+
+    
+    public static void main(String[] args) {
+        Janela j = new Janela();
+        j.setVisible(true);
+    }
+    
+}
---
+++ Aula_POO/Aula12_ex2/src/aula12_ex2/Aula12_ex2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula12_ex2;
+
+/**
+ *
+ * @author katym
+ */
+public class Aula12_ex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Janela j = new Janela();
+       j.setVisible(true);
+    }
+    
+}
